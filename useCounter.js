649b36271e364172f1b8fc333ff522941957280c
Rieral <fabiancondori9@gmail.com>
2022-05-09 14:32:09 -0400

    Hook

---
+++ useCounter.js
@@ -0,0 +1,36 @@
+import { useState } from "react"
+
+
+export const useCounter = ( initialState = 10) => {
+  const [counter, setCounter] = useState(initialState);
+  const increment = () =>{
+      setCounter ( counter + 1)
+  }
+  const decrement = () =>{
+    setCounter ( counter - 1)
+  }
+  const reset = ()=>{
+      setCounter (initialState)
+  }
+  return{
+      counter,
+      increment,
+      decrement,
+      reset
+  }
+  // const increment = ( factor = 1) =>{
+  //   setState ( state + factor )
+  // }
+  // const decrement = (factor = 1) =>{
+  //   setState ( state - factor )
+  // }
+  // const reset = ()=>{
+  //     setState (initialState)
+  // }
+  // return{
+  //     state,
+  //     increment,
+  //     decrement,
+  //     reset
+  // }
+}
